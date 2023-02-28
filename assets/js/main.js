29dcfea63db191207713123c23467aadfc434e1f
Midhun Nair <midhunnair01@yahoo.com>
2023-02-28 23:45:50 +0530

    Added all Files

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+function openMenu() {
+
+    if(screen.width < 1000)
+    {
+        let menu = document.querySelector(".menuBar");
+        menu.style="width:80%";
+        document.querySelector(".headbar").style="z-index:2"
+        document.querySelector(".overlay").style="display:block";
+        document.querySelector(".menuCloseBtn").style="display:block";
+    }
+
+    else{
+        let menu = document.querySelector(".menuBar");
+        menu.style="width:25%";
+        document.querySelector(".headbar").style="z-index:2"
+        document.querySelector(".overlay").style="display:block";
+        document.querySelector(".menuCloseBtn").style="display:block";
+    }
+}
+
+function closeMenu() {
+    let menu = document.querySelector(".menuBar");
+    menu.style="width:0%";
+    document.querySelector(".headbar").style="z-index:4"
+    document.querySelector(".overlay").style="display:none";
+    document.querySelector(".menuCloseBtn").style="display:none";
+}
+
+function scrollNext(e) {
+    e.target.parentNode.parentNode.scrollLeft += 250;
+    e.target.parentNode.parentNode.parentNode.scrollLeft += 250;
+    
+    // e.target.parentNode.parentNode.scrollLeft += 200;
+}
+function scrollPrev(e) {
+    e.target.parentNode.parentNode.scrollLeft -= 250;
+    e.target.parentNode.parentNode.parentNode.scrollLeft -= 250;
+}
+
+function showSigninDropDown() {
+    document.getElementById("account-DropDown").style.display="block";
+    document.querySelector(".overlay").style="display:block";
+}
+
+function hideSigninDropDown() {
+    document.getElementById("account-DropDown").style.display="none";
+    document.querySelector(".overlay").style="display:none";
+}
+
+function showFilter(e) {
+    console.log(e)
+    document.getElementById("mobFilterDropDown").style.display="block";
+}
+function hideFilter() {
+    document.getElementById("mobFilterDropDown").style.display="none";
+}
+
+let cartCount=0;
+
+function cartCnt(){
+cartCount++;
+document.querySelector(".cartCount").innerHTML=cartCount;
+}
